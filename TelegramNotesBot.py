62bd2b46ae4c9439c91c337eaae765bfa400e8ae
Umerenkov <umerenkov21@gmail.com>
2019-03-19 20:35:06 +0300

    Keyboard menus in progress

--- TelegramNotesBot.py
+++ TelegramNotesBot.py
@@ -1,39 +1,49 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import telegram
 import sqlite3
 import logging
 import os
-import telegram as tg
 
 # Bot token
-TOKEN = '#########:###################################'
+TOKEN = '#########:########################'
 
 # Proxy settings for things to work in Russia
+'''
 REQUEST_KWARGS = {
     'proxy_url': 'socks5://142.93.232.142:####',
     'urllib3_proxy_kwargs': {
-        'username': '########',
-        'password': '########',
+        'username': '##########',
+        'password': '##########',
     }
 }
+'''
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-bot = tg.Bot(TOKEN)
-print(bot.get_me())
+bot = telegram.Bot(TOKEN)
 
-help_message = 'This bot is intended for collecting and storing notes for your needs. \n'
-                'Click <b>\"New Note\"</b> button to take a new note, \n'
-                'Click <b>\"List of Notes\"</b> button to view the list of taken notes, \n'
-                'Click <b>\"Show\"</b> button to view a particular note. You will be asked the date and time when'
-                'the note you need was taken. \n'
-                'Enter <b>\\help</b> command to view this message again. \n'
+help_message = '''This bot is intended for collecting and storing notes for your needs. \n
+Click *\"✍ New Note\"* button to take a new note, \n
+Click *\"👀 Show\"* button to view a particular note,
+You will be asked any part of the needed note or the date or time of its creation. \n
+Click *\"🗒 List of Notes\"* button to view the list of taken notes. \n
+Enter */help* command to view this message again. \n'''
 
 
-def start(bot, update):
+def start(update, context):
     # Send a message when the command /start is issued.
-    update.message.reply_text('<b>Greetings, traveler!</b> \n \n' + help_message)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text='*Greetings, traveler!* \n' + help_message, parse_mode=telegram.ParseMode.MARKDOWN)
+
+    keyboard = [[InlineKeyboardButton("✍ New Note", callback_data='/take_note'),
+                 InlineKeyboardButton("👀 Show", callback_data='/show')],
+                [InlineKeyboardButton("🗒 List of Notes", callback_data='/notes_list')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose one of the options:', reply_markup=reply_markup)
 
     # Creates a brand new database file in case of absebse of any .db files in current folder
     if not os.path.exists(os.path.dirname(__file__) + '/*.db'):
@@ -41,12 +51,27 @@ def start(bot, update):
             pass
 
 
-def help(bot, update):
+def callbackHandler(bot, update, chat_data):
+    callbackquery = update.callback_query
+    print(callbackquery)
+    querydata = callbackquery.data
+    print(querydata)
+    '''
+    if not 'lang' in  chat_data:
+        getChatFile(chat_data, update.message.chat.id)
+        if not 'lang' in chat_data:
+            chat_data['lang'] = ""
+    if not querydata:
+        return
+    '''
+
+def help(update, context):
     # Send the help message when the command /help is issued.
-    update.message.reply_text(help_message)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=help_message, parse_mode=telegram.ParseMode.MARKDOWN)
 
 
-def take_note(bot, update):
+def take_note(update, context):
     # Writing a new note into the database.
     update.message.reply_text('Please write the body of the new note: ')
 
@@ -57,8 +82,9 @@ def take_note(bot, update):
     conn.commit()
     conn.close()
 
+    update.message.reply_text('✅ Your note has been succesfully taken.')
 
-def notes_list(bot, update):
+def notes_list(update, context):
     # Returns a list of taken notes
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
@@ -69,10 +95,10 @@ def notes_list(bot, update):
     # Stripping the first lines of notes and printing them with some formatting and corresponding dates
     notes = [(note[:note.find('\n')].strip() + '...' if len(note) > 60 else note, date) for note, date in notes]
     for note, date in notes:
-        update.message.reply_text(date + '\n <b>' + note.ljust(60) + '</b>' + '\t \t \n')
+        update.message.reply_text(date + '\n *' + note.ljust(60) + '*' + '\t \t \n')
 
 
-def show(bot, update):
+def show(update, context):
     # Show the particular note
     update.message.reply_text('Please write any part of the needed note or the date or time of its creation: \n'
                               'for example: "do not forget to turn off the iron", "16:20" or "05.01.2009"')
@@ -91,11 +117,11 @@ def show(bot, update):
     if not candidates:
         # if empty
         update.message.reply_text('Sorry, we couldn\'t find such note! \n'
-                                  'Press <b>\"List of Notes\"</b> button to get valid options.')
+                                  'Press *\"List of Notes\"* button to get valid options.')
 
     elif len(candidates) == 1:
         # if there's the one, print a date and the note itself.
-        update.message.reply_text(candidates[0][1] + '\n <b>' + candidates[0][0].ljust(60) + '</b>' + '\t \t \n')
+        update.message.reply_text(candidates[0][1] + '\n *' + candidates[0][0].ljust(60) + '*' + '\t \t \n')
 
     else:
         # if there's >1 of them, Print a number of candidate, date and the note itself.
@@ -103,9 +129,9 @@ def show(bot, update):
         for index in range(len(candidates)):
             _note = candidates[index][0]
             _date = candidates[index][1]
-            update.message.reply_text(_date + '\n <b>' + str(index + 1) + '. ' +
+            update.message.reply_text(_date + '\n *' + str(index + 1) + '. ' +
                                       (_note[:_note.find('\n')].strip() + '...' if len(_note) > 60 else _note).ljust(
-                                          60) + '</b>' + '\t \t \n')
+                                          60) + '*' + '\t \t \n')
 
         update.message.reply_text('Please choose one of the options, printing out its number: ')
         query = update.message.text
@@ -115,30 +141,18 @@ def show(bot, update):
 
         if index not in range(len(candidates)):
             update.message.reply_text('Sorry, we couldn\'t find such note! \n'
-                                      'Press <b>\"List of Notes\"</b> button to get valid options.')
+                                      'Press *\"List of Notes\"* button to get valid options.')
         else:
-            update.message.reply_text(candidates[index][1] + '\n <b>' + str(index + 1) + '. ' +
-                                      candidates[index][0].ljust(60) + '</b>' + '\t \t \n')
+            update.message.reply_text(candidates[index][1] + '\n *' + str(index + 1) + '. ' +
+                                      candidates[index][0].ljust(60) + '*' + '\t \t \n')
 
 
-def build_menu(buttons,
-               n_cols,
-               header_buttons=None,
-               footer_buttons=None):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, header_buttons)
-    if footer_buttons:
-        menu.append(footer_buttons)
-    return menu
-
-
-def error(bot, update, error):
+def error(update, context):
     # Log Errors caused by Updates.
-    logger.warning('Update %s caused error %s', update, error)
+    logger.warning('Update %s caused error %s', update, context.error)
 
 
-def unknown(bot, update):
+def unknown(update, context):
     # Send a message when the command /help is issued.
     update.message.reply_text('Sorry, I didn\'t understand that command!')
 
@@ -146,11 +160,13 @@ def unknown(bot, update):
 def main():
     # Start the bot.
     # Create the EventHandler and get the dispatcher to register handlers
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
+
+    dp.add_handler(CallbackQueryHandler(callbackHandler))
     dp.add_handler(CommandHandler('take_note', take_note))
     dp.add_handler(CommandHandler('notes_list', notes_list))
     dp.add_handler(CommandHandler('show', show))
@@ -158,12 +174,13 @@ def main():
     '''
     # menu
     button_list = [
-        tg.InlineKeyboardButton('col1', callback_data=...),
-        tg.InlineKeyboardButton('col2', callback_data=...),
-        tg.InlineKeyboardButton('row 2', callback_data=...) 
-        #callback_data: str – Optional. Data to be sent in a callback query to the bot when button is pressed, 1-64 bytes.
+        telegram.InlineKeyboardButton('col1', callback_data=...),
+        telegram.InlineKeyboardButton('col2', callback_data=...),
+        telegram.InlineKeyboardButton('row 2', callback_data=...) 
+        # callback_data: str – Optional. 
+        # Data to be sent in a callback query to the bot when button is pressed, 1-64 bytes.
     ]
-    reply_markup = tg.InlineKeyboardMarkup(util.build_menu(button_list, n_cols=2))
+    reply_markup = telegram.InlineKeyboardMarkup(util.build_menu(button_list, n_cols=2))
     bot.send_message(..., 'A two-column menu', reply_markup=reply_markup)
     '''
 
@@ -180,3 +197,7 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+
+if __name__ == '__main__':
+    main()
